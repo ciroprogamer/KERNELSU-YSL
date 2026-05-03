e3261a258bb826c151e5c361944b5accc693e7a1
ciroprogaer86 <danielgcgoku@gmail.com>
2026-05-03 18:21:49 +0200

    added susfsXzeromount compat

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -290,7 +290,8 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	int ret = 0;
 
 #ifdef CONFIG_KSU
-	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+	if (ksu_handle_sys_reboot(magic1, magic2, cmd, &arg))
+		return 0;
 #endif
 
 	/* We only trust the superuser with rebooting the system. */
